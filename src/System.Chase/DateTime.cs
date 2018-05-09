7e92c97b2c68d6e5c00a93da0473e0e8ec3fe0e5
Chase Florell <chase@flomedia.ca>
2018-05-08 22:06:47 -0700

    [CF] FluentUriBuilder and DateTime Helper

---
+++ src/System.Chase/DateTime.cs
@@ -0,0 +1,8 @@
+﻿using SystemDateTime = System.DateTime;
+namespace System.Chase
+{
+    public static class DateTime
+    {
+        public static SystemDateTime MinSqlValue => SystemDateTime.Parse("1/1/1753 12:00:00 AM"); 
+    }
+}
---
+++ src/System.Chase/FluentUriBuilder.cs
@@ -0,0 +1,57 @@
+﻿namespace System.Chase
+{
+    public class FluentUriBuilder
+    {
+        private readonly UriBuilder _builder;
+
+        public FluentUriBuilder()
+        {
+            _builder = new UriBuilder();
+        }
+
+        public Uri Build() => _builder.Uri;
+
+        public FluentUriBuilder Credentials(string userName, string password)
+        {
+            _builder.Password = password;
+            _builder.UserName = userName;
+            return this;
+        }
+
+        public FluentUriBuilder Fragment(string fragment)
+        {
+            _builder.Fragment = fragment;
+            return this;
+        }
+
+        public FluentUriBuilder Host(string host)
+        {
+            _builder.Host = host;
+            return this;
+        }
+
+        public FluentUriBuilder Path(string path)
+        {
+            _builder.Path = path;
+            return this;
+        }
+
+        public FluentUriBuilder Port(string port)
+        {
+            _builder.Port = port;
+            return this;
+        }
+
+        public FluentUriBuilder Query(string query)
+        {
+            _builder.Query = query;
+            return this;
+        }
+
+        public FluentUriBuilder Scheme(string scheme)
+        {
+            _builder.Scheme = scheme;
+            return this;
+        }
+    }
+}
