fe9c73dfef07d20295227ac4a13196e3bfee1e61
Chase Florell <chase@flomedia.ca>
2018-05-08 22:28:42 -0700

    [CF] building up new FluentUrilBuilder and Tests

---
+++ tests/System.Chase.Tests/Unit/FluentUriBuilderTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace System.Chase.Tests.Unit
+{
+    [TestFixture]
+    public class FluentUriBuilderTests
+    {
+        private FluentUriBuilder _builder;
+
+        public void SetUp()
+        {
+            _builder = new FluentUriBuilder();
+        }
+        [Test]
+        public void ShouldBuildUriWithFragment()
+        {
+            // setup
+            var expectedPort = 80;
+            var expectedHost = "example.com";
+            var fragment = "frag";
+            var root = "http://example.com";
+            var builder = new FluentUriBuilder(root);
+            
+            // execute
+            var result = builder.Fragment(fragment)
+                .Build();
+
+            // assert
+            result.Fragment.Should().Be($"#{fragment}");
+            result.Port.Should().Be(expectedPort);
+            result.Host.Should().Be(expectedHost);
+        }
+
+        [Test]
+        public void ShouldBuildUriAndChangePort()
+        {
+            // setup
+            var expectedPort = 8080;
+            var expectedHost = "example.com";
+            var fragment = "frag";
+            var root = "http://example.com";
+            var builder = new FluentUriBuilder(root);
+            
+            // execute
+            var result = builder.Fragment(fragment)
+                .Port(expectedPort)
+                .Build();
+
+            // assert
+            result.Fragment.Should().Be($"#{fragment}");
+            result.Port.Should().Be(expectedPort);
+            result.Host.Should().Be(expectedHost);
+        }
+    }
+}
