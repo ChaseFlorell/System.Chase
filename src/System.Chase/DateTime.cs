5b71dffb7bd780c5cdb4035e1f485ff91feacca4
Chase Florell <chase.florell@gmail.com>
2018-06-27 16:03:17 -0700

    [CF] added linux epoch value to DateTime helper

--- src/System.Chase/DateTime.cs
+++ src/System.Chase/DateTime.cs
@@ -1,8 +1,8 @@
-﻿using SystemDateTime = System.DateTime;
-namespace System.Chase
+﻿namespace System.Chase
 {
     public static class DateTime
     {
-        public static SystemDateTime MinSqlValue => SystemDateTime.Parse("1/1/1753 12:00:00 AM"); 
+        public static System.DateTime MinSqlValue => System.DateTime.Parse("1/1/1753 12:00:00 AM");
+        public static System.DateTime Epoch => System.DateTime.Parse("1/1/1970 12:00:00 AM");
     }
 }
